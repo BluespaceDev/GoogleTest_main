0f8e79fadf9076b6bcd78bac8110d437cb5f5596
bluespace <bluespace.dev@gmail.com>
2021-04-15 09:24:27 +0900

    Add files via upload

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+
+
+#include <limits.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using testing::InitGoogleTest;
+
+namespace
+{
+
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
